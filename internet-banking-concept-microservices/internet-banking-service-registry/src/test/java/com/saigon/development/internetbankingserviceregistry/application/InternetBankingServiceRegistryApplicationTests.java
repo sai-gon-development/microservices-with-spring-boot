58a267e61b9b9ff013c09a0169f5a9faf20c73b5
Richard Vu <richardvu.work@gmail.com>
2022-05-31 15:00:11 +0700

    Initial Project

---
+++ internet-banking-concept-microservices/internet-banking-service-registry/src/test/java/com/saigon/development/internetbankingserviceregistry/application/InternetBankingServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saigon.development.internetbankingserviceregistry.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetBankingServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
