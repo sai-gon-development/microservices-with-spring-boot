3fb517d437ad5884b75bb2905bdef4c357cb01ee
Richard Vu <richardvu.work@gmail.com>
2022-06-01 12:38:01 +0700

    Initial project

---
+++ internet-banking-microservices/maven/internet-banking-config-server/src/main/java/com/saigon/development/application/InternetBankingConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.saigon.development.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetBankingConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetBankingConfigServerApplication.class, args);
+	}
+
+}
