42de9df4d6357dab6e6f864a3996063577507ff3
Luong Vu <luong.vu@bnksolution.com>
2022-06-02 13:26:22 +0700

    Update config

--- internet-banking-microservices/maven/internet-banking-config-server/src/main/java/com/saigon/development/application/InternetBankingConfigServerApplication.java
+++ internet-banking-microservices/maven/internet-banking-config-server/src/main/java/com/saigon/development/application/InternetBankingConfigServerApplication.java
@@ -3,13 +3,14 @@ package com.saigon.development.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @EnableConfigServer
 @SpringBootApplication
 public class InternetBankingConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InternetBankingConfigServerApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(InternetBankingConfigServerApplication.class, args);
+  }
 }
